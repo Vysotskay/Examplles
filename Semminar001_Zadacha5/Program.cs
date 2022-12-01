9d4dd2806f476ec36954aa59201eb5ac27ac0c66
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-01 21:21:31 +0300

    Semminar 001 Zadacha 5

---
+++ Semminar001_Zadacha5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Введите число N и выведите все числа от -N до N
+Console.Clear();
+Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+int count = number * -1;
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
