ee5d2fc31dc0dda3fc703e2b2c95e025ee10771b
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-08 21:24:28 +0300

    Semminar 002

---
+++ Semminar002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23 -
+// Делиться и на 7 и на 23 без остатка
+
+Console.Clear();
+Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 7 == 0 & number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+ 
