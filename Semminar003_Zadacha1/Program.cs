d6ae4b210ac1f7c50cd00895f6e0f90f268671f9
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-17 16:43:09 +0300

    Semminar 003 Z 1

---
+++ Semminar003_Zadacha1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Создание объекта для генерации чисел
+Random rnd = new Random();
+
+// Получить случайное число ( в диапозоне от 10 до 99)
+int value = rnd.Next(10, 99);
+
+// Вывод числа в консоль
+Console.WriteLine(value);
+
+int des = value/10;
+Console.WriteLine(des);
+int ed = value%10;
+Console.WriteLine(ed);
+if (des >ed)
+{
+    Console.WriteLine($"наибольшее число {des}" );
+}
+else
+{
+    Console.WriteLine($"наибольшее число {ed}" );
+}
