11b5d60164b75233761fa2e56f0548a7626f3abb
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-17 18:17:33 +0300

    Semminar 003 Z 2

---
+++ Semminar003_Zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+Random rnd = new Random();
+int value = rnd.Next(100, 1000);
+Console.WriteLine(value);
+int R = value%10;
+int R1 = value/100;
+int result = R1*10 + R;
+Console.WriteLine(result);
