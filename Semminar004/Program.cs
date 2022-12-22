f200a9a2511af755300d11bdc94727c3990f8f1f
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-22 21:16:13 +0300

    Semminar 004 z1

---
+++ Semminar004/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите прорамму, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+//Console.Write("Введите чсло: ");
+//int num = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//int sum1 = GetSumNums(num);
+//int sum2 = GetSumNums(num2);
+//Console.WriteLine(sum1);
+//Console.WriteLine(sum2);
+
+//int GetSumNums(int number)
+//{
+    //int sum = 0;
+    //while(number > 0)
+    //{
+        //sum += number;
+        //number--;
+         //Console.Write($"{sum} ");
+    //}
+    //Console.WriteLine();
+    //return sum;
+//}
+
+
+
+
+Console.Write("Введите чсло: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)} ");
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number;
+        number--;
+         Console.Write($"{sum} ");
+    }
+    Console.WriteLine();
+    return sum;
+}
