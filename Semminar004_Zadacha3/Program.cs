8fbc74cb22d1ea2c6a1e2cee0115e51fb95f43f7
Vysotskay <115286661+Vysotskay@users.noreply.github.com>
2022-12-23 15:24:42 +0300

    Semminar004 Z3

---
+++ Semminar004_Zadacha3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int GetCompNums(int number)
+{
+    int comp = 1;
+    while(number > 1)
+    {
+        comp = comp * number;
+        number--;
+        Console.WriteLine($"{comp}");
+    }
+
+    Console.Write();
+    return comp;
+}
